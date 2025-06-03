35e4bb99061217a089765e6da86e7241f30144b8
contactvrajpatel18 <136819712+contactvrajpatel18@users.noreply.github.com>
2025-06-03 19:00:51 +0530

    two

---
+++ Javascript/Day_02/script.js
@@ -0,0 +1,11 @@
+// let a = 2;
+// let a = 3;
+const a = 2;
+alert(a); // This will alert 3, as the second declaration overwrites the first one
+
+
+ 
+var abcd = "This is a simple HTML document.";
+// document.querySelector("p").textContent = abcd;
+document.getElementById("a").textContent = abcd;
+document.getElementById("b").textContent = abcd;
